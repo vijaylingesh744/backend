52bccba1c91a04620da03786fa73a2874e4e5a6f
Lingeswaran <113828454+vijaylingesh744@users.noreply.github.com>
2024-10-03 14:55:54 +0530

    Update ApiRoute.js

--- frontend/src/componant/utils/ApiRoute.js
+++ frontend/src/componant/utils/ApiRoute.js
@@ -7,7 +7,7 @@ module.exports = {
   BUSINESS_URL:"http://connect-souq.qcodesinfotech.com/",
   APIBASE_URL:"https://3.27.202.97/api",
   BASE_URL:"https://3.27.202.97/",
-  APPURL:"https://3.27.202.97",
+  APPURL:"https://3.27.202.97/",
 
   // BUSINESS_URL:"http://localhost:3001/",
   // APIBASE_URL:"http://localhost:5000/api",
